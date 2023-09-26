408f4b0b4515ca36d20460e26f3e05df57171434
HusseinSerag <117987162+HusseinSerag@users.noreply.github.com>
2023-09-26 18:47:07 +0300

    Add game function and complete the rock case
    game function is added with a functionality that enables a person to
    play as much rounds as inputted using prompt and doesn't allow
    non-integer inputs ,and the 'rock' player choice has been completed and
    tested

--- javascript.js
+++ javascript.js
@@ -7,13 +7,47 @@ function getComputerChoice(){
 function playRound(playerSelection,ComputerSelection){
     const playerSelectionCmp = playerSelection.toLowerCase();
     
+    
     if (playerSelectionCmp === 'rock'){
         if(ComputerSelection === 'rock'){
             return 'draw!'
         }
+        if(ComputerSelection === 'scissors'){
+            return 'You won!, Rock crushes scissors!'
+        }
+        else{
+            return 'you lost! , paper slips rock!'
+        }
+    }
+    
+    else{
+        return 'wrong input'
     }
 }
 
 
 
 
+function game(){
+    let numberOfGames;
+    do
+
+    {
+        numberOfGames = prompt('How many rounds?')
+        try{
+            numberOfGames = Number(numberOfGames)
+        }
+        catch{
+            
+        }
+    }
+    while(!Number.isInteger(numberOfGames) || numberOfGames === null || numberOfGames === undefined || numberOfGames === 0)
+
+    
+    for(let counter = 0 ; counter < numberOfGames ; counter++){
+
+        console.log(playRound(prompt('Your choice'),getComputerChoice()))
+    }
+}
+
+game()
