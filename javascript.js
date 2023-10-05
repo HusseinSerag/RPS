3370aedee2085088d4f55a36077503fbc2d7d035
HusseinSerag <117987162+HusseinSerag@users.noreply.github.com>
2023-10-05 19:43:01 +0300

    Add functionality but inside HTML file with no css
    Buttons for RPS and winning and losing functionality has been added,
    text appears who won and who lost has been added

--- javascript.js
+++ javascript.js
@@ -7,7 +7,7 @@ function getComputerChoice(){
 function playRound(playerSelection,ComputerSelection){
     const playerSelectionCmp = playerSelection.toLowerCase();
     
-    console.log(`Your choice :${playerSelection} , Computer Choice : ${ComputerSelection} `)
+    choices.textContent = `Your choice : ${playerSelection} , Computer Choice : ${ComputerSelection} `
     
     if (playerSelectionCmp === 'rock'){
 
@@ -31,66 +31,88 @@ function playRound(playerSelection,ComputerSelection){
 
 function rockChoice(ComputerSelection){
     if(ComputerSelection === 'rock'){
-        return 'draw!'
+        return ['draw!',1]
     }
     if(ComputerSelection === 'scissors'){
-        return 'You won!, Rock crushes scissors!'
+        return ['You won!, Rock crushes scissors!',2]
     }
     else{
-        return 'you lost! , paper slips rock!'
+        return ['you lost! , paper slips rock!',0]
     }
 }
 
 function paperChoice(ComputerSelection){
     if(ComputerSelection === 'paper'){
-        return 'draw';
+        return ['draw!',1]
     }
     else if(ComputerSelection === 'rock'){
-        return 'You won! , paper slips rock!';
+        return ['You won! , paper slips rock!',2]
     }
     else{
-        return 'you lost! , scissors cut paper!'
+        return ['you lost! , scissors cut paper!',0]
     }
 }
 
 function scissorsChoice(ComputerSelection){
     if(ComputerSelection === 'scissors'){
-        console.log('draw!')
+        return ['draw!',1]
 
     }
     else if(ComputerSelection === 'paper'){
-        console.log('You won! , Scissors cuts paper')
+        return ['You won! , Scissors cuts paper',2]
     }
     else{
-        console.log('You lost! , rock crushed scissors')
+        return ['You lost! , rock crushed scissors',0]
+    }
+}
+
+function gameover(Winner){
+    buttons.childNodes.forEach(btn =>{
+        btn.disabled = true
+    })
+    if(Winner == 'P'){
+        choices.textContent = 'You won';
+    }
+    else{
+        choices.textContent = 'Computer won'
     }
 }
 
 
-
-
-function game(){
-    let numberOfGames;
-    do
-
-    {
-        numberOfGames = prompt('How many rounds?')
-        try{
-            numberOfGames = Number(numberOfGames)
+function game(PlayersChoice){
+   
+        let arr =  playRound(PlayersChoice,getComputerChoice());
+        text.textContent = arr[0];
+        let score = arr[1]
+        if(score === 2){
+            let playerScore = Number(player.textContent)
+            player.textContent = playerScore+1
+            if(player.textContent == '5'){
+                gameover('P')
+            }
         }
-        catch{
-            
+        else if(score == 0){
+            let compScore = Number(computer.textContent)
+            computer.textContent = compScore+1
+            if(computer.textContent == '5'){
+                gameover('C')
+            }
         }
-    }
-    while(!Number.isInteger(numberOfGames) || numberOfGames === null || numberOfGames === undefined || numberOfGames === 0)
-
+        
     
-    for(let counter = 0 ; counter < numberOfGames ; counter++){
-        console.group(`Round ${counter+1}`)
-        console.log(playRound(prompt('Your choice'),getComputerChoice()))
-        console.groupEnd(`Round ${counter+1}`)
-    }
     
 }
 
-game()
+
+
+const buttons = document.querySelector('.game');
+const text = document.querySelector('.text');
+const choices = document.querySelector('.choices')
+const player = document.querySelector('.player')
+const computer = document.querySelector('.computer')
+
+buttons.addEventListener('click', e =>{
+    e.target.id === 'rock' ? game('rock') : e.target.id === 'paper' ? game('paper') : e.target.id === 'scissors' ? game('scissors') : '';
+    
+});
+
